a166eab41e172740dc5dae85fb87cec3987d5da5
Jack Oddy <jackoddy91@gmail.com>
2016-07-18 14:39:27 +0100

    adds cat photo page to sinatra

--- app.rb
+++ app.rb
@@ -3,3 +3,21 @@ require 'sinatra'
 get ('/') do
   'hello world'
 end
+
+get ('/secret') do
+  "you're handsome"
+end
+
+get ('/truth') do
+  'cold potatoes ain\'t hot'
+end
+
+get('/cat') do
+  "<style>
+  img{
+    border: dashed;
+    border-color: red;
+  }
+  </style>
+    <img src='http://bit.ly/1eze8aE'>"
+end
