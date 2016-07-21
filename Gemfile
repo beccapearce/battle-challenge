34b1c1ea6451a1b01b3b617ef218a5e8a4da9c9f
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-07-21 09:54:03 +0100

    first commit of day 4

--- Gemfile
+++ Gemfile
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
-# A sample Gemfile
+
 source "https://rubygems.org"
 
-ruby "2.3.0"
-
-gem "sinatra"
-gem "rspec-sinatra"
-gem "capybara"
+gem 'sinatra'
+gem 'capybara'
+gem 'selenium-webdriver'
+gem 'shotgun'
+gem 'rspec-sinatra'
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -5,26 +5,26 @@ describe Player do
   subject(:mittens) { Player.new('Mittens') }
 
   describe '#name' do
-    it 'returns the name' do
+    it 'Returns the name' do
       expect(dave.name).to eq 'Dave'
     end
   end
 
   describe '#hit_points' do
-    it 'returns the hit points' do
+    it 'Returns the hit points' do
       expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
     end
   end
 
   describe '#attack' do
-    it 'damages the player' do
+    it 'Damages the player' do
       expect(mittens).to receive(:receive_damage)
       dave.attack(mittens)
     end
   end
 
   describe '#receive_damage' do
-    it 'reduces the player hit points' do
+    it "Reduces the player's hit points" do
       expect { dave.receive_damage }.to change { dave.hit_points }.by(-10)
     end
   end
