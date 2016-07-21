6dacdf046a11e7aeab2e1cbd1fc401f6be3fb795
Rebecca Pearce <becciepearce@hotmail.co.uk>
2016-07-21 15:12:04 +0100

    completes tests for turn switching and damage taking

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -5,21 +5,29 @@ feature 'Attack' do
 
   scenario 'reduce Player 2 HP by 10' do
     sign_in_and_play
-    click_link 'Attack'
-    click_link 'OK'
+    attack_and_confirm
     expect(page).not_to have_content 'Mittens: 60HP'
     expect(page).to have_content 'Mittens: 50HP'
   end
 
-  scenario 'reduce Player 1 HP by 10' do
+  scenario 'Confirm player one attack on player two' do
     sign_in_and_play
     click_link 'Attack'
-    click_link 'OK'
-    click_link 'Attack'
-    click_link 'OK'
+    expect(page).to have_content 'Dave attacked Mittens'
+  end
+
+  scenario 'reduce Player 1 HP by 10' do
+    sign_in_and_play
+    attack_and_confirm
+    attack_and_confirm
     expect(page).not_to have_content 'Dave: 60HP'
     expect(page).to have_content 'Dave: 50HP'
   end
 
-
+  scenario 'Confirm player two attack on player one' do
+    sign_in_and_play
+    attack_and_confirm
+    click_link 'Attack'
+    expect(page).to have_content 'Mittens attacked Dave'
+  end
 end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -5,4 +5,8 @@ feature "View hit points" do
     sign_in_and_play
     expect(page).to have_content 'Mittens: 60HP'
   end
+  scenario "See player 1 hit points" do
+    sign_in_and_play
+    expect(page).to have_content 'Dave: 60HP'
+  end
 end
